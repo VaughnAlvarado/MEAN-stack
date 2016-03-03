713273119bb00520f636d96694b7dbacf136a045
Vaughn <alvaradovaughn@yahoo.com>
2016-03-03 14:31:02 -0600

    function to trigger the edits

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -7,16 +7,70 @@ module.exports.updatePhoto = function (req, res) {
 	var userId = req.body.userId;
 
 	console.log("User " + userId + "is submitting " , file);
-	var uploadDate = new Date().toISOString;
-	uploadDate = uploadDate.replace(".", "");
-	uploadDate = uploadDate.replace("-", "");
-	uploadDate = uploadDate.replace(":", "");
-
+	var uploadDate = new Date();
+	
 
 	var tempPath = file.path;
-	var targetPath = path.join(__dirname, "../../uploads" + userId + uploadDate + file.name);
+	var targetPath = path.join(__dirname, "../../uploads/" + userId + uploadDate + file.name);
+	var savePath = "/uploads/" + userId + uploadDate + file.name
 
-	fs.rename(tempPath, targetPath, function (err, data) {
+	fs.rename(tempPath, targetPath, function (err) {
+		if (err) {
+			console.log(err);
+		} else {
+			User.findById(userId, function(err, userData) {
+				var user = userData;
+				user.image = savePath;
+				user.save(function(err) {
+					if (err) {
+						console.log("failed to save");
+						res.json({status: 500})
 
+					} else {
+						console.log("saved succesfully");
+
+						res.json({status: 200})
+					}
+				})
+			})
+		}
 	})
+};
+module.exports.updateUsername = function(req, res){
+	var username = req.body.username;
+	var userId = req.body.userId;
+
+	User.findById(userId, function(err, userData) {
+		var user = userData;
+		user.username = username;
+
+		user.save(function(err) {
+			if (err) {
+				console.log("fail");
+				res.json({status: 500});
+			} else {
+				console.log("success");
+				res.json({status: 200});
+			}
+		})
+	});
+}
+module.exports.updateBio = function(req,res) {
+	var bio = req.body.bio;
+	var userId = req.body.userId;
+
+	User.findById(userId, function(err, userData) {
+		var user = userData;
+		user.bio = bio;
+
+		user.save(function(err) {
+			if (err) {
+				console.log("fail");
+				res.json({status: 500});
+			} else {
+				console.log("success");
+				res.json({status: 200});
+			}
+		})
+	});	
 }
