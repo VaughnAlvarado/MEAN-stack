ea262575de6b9ba9b8e7d07fdacfc167bb458731
Vaughn <alvaradovaughn@yahoo.com>
2016-03-03 12:45:23 -0600

    changing function name ands now making every image profile path unique

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -3,8 +3,20 @@ var fs = require('fs-extra');
 var path = require('path');
 
 module.exports.updatePhoto = function (req, res) {
-	var file = req.body.file;
-	var userID = req.body.userId;
+	var file = req.files.file;
+	var userId = req.body.userId;
 
 	console.log("User " + userId + "is submitting " , file);
+	var uploadDate = new Date().toISOString;
+	uploadDate = uploadDate.replace(".", "");
+	uploadDate = uploadDate.replace("-", "");
+	uploadDate = uploadDate.replace(":", "");
+
+
+	var tempPath = file.path;
+	var targetPath = path.join(__dirname, "../../uploads" + userId + uploadDate + file.name);
+
+	fs.rename(tempPath, targetPath, function (err, data) {
+
+	})
 }
