a0611789b9973876ccc7c33f726961fe4c6ef94e
Vaughn <alvaradovaughn@yahoo.com>
2016-03-03 14:31:29 -0600

    users can now enter a bio and username

--- server/datasets/users.js
+++ server/datasets/users.js
@@ -2,5 +2,8 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', {
 	email: String, 
-	password: String
+	password: String,
+	image: String,
+	username: String,
+	bio: String
 });
