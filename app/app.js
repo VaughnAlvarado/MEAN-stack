f19e00811a002737c4dd70d45faab4e04597bec3
Vaughn <alvaradovaughn@yahoo.com>
2016-03-02 21:06:57 -0600

    app for the mean stack social network

---
+++ app/app.js
@@ -0,0 +1,17 @@
+(function(){
+	angular.module('SocialNetwork', ['ui.router', 'ngFileUpload'])
+			.config(function($stateProvider) {
+
+			$stateProvider
+				.state('signUp', {
+				url: "/signup",
+				templateUrl: "app/signup/signup.html",
+				controller: "SignupController"
+			})
+				.state('editProfile', {
+					url: "/edit-profile",
+					templateUrl: "app/profile/edit-profile-view.html",
+					controller:"EditProfileController"
+				})
+		})
+}());
